9bf13054d0c71fcec9c787f6a49cbeab2e1d738d
MariNists <nistyuk.mari@gmail.com>
2023-11-08 05:02:39 +0300

    fixed header

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,7 +6,6 @@ export const swiper = new Swiper('.swiper-container', {
     // Optional: Configuration options here
     modules: [Navigation, Pagination],
     slidesPerView: 1,
-    spaceBetween: 30,
     loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
